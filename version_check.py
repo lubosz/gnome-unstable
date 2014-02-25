d4f090f416a5a35c87a9c9bb99c800b1ffc17038
Lubosz Sarnecki <lubosz@gmail.com>
2014-02-25 02:36:36 +0100

    add some package mappings

--- version_check.py
+++ version_check.py
@@ -6,7 +6,23 @@ from bs4 import BeautifulSoup
 from IPython import embed
 
 arch_to_gnome = {
-  "gconf": "GConf"
+  "gconf": "GConf",
+  "gdk-pixbuf2": "gdk-pixbuf",
+  "glib2": "glib",
+  "gphoto2": "gphoto",
+  "gtk3": "gtk",
+  "gtkhtml4": "gtkhtml",
+  "gtkmm3": "gtkmm",
+  "gtksourceview3" : "gtksourceview",
+  "pygtksourceview2" : "pygtksourceview",
+  "vte3": "vte",
+  "totem-plparser": "totem-pl-parser",
+  "networkmanager" : "NetworkManager"
+}
+
+pkgname_to_arch = {
+  "pygobject": "python-gobject",
+  "pyatspi" : "python-atspi"
 }
 
 GREEN = '\033[92m'
@@ -63,11 +79,13 @@ def get_largest_minor_version(url, package):
     for link in links:
         link_url = link.attrs["href"].replace("/", "")
         if package in link_url:
-            version = link_url.split("-")[-1].replace(".tar.xz", "").replace(".tar.gz", "")
+            version = link_url.split("-")[-1].replace(".tar.xz", "").replace(".tar.gz", "").replace(".tar.bz2", "").replace(".sha256sum", "")
             
     return version
 
 def get_upstream_version(package):
+
+    package = package.strip()
 
     if package in arch_to_gnome:
         package = arch_to_gnome[package]
@@ -78,11 +96,17 @@ def get_upstream_version(package):
     if major_version == "N/A":
         return "N/A"
 
-    url += "/" + major_version
+    url += "/" + major_version + "/?C=M;O=A"
     minor_version = get_largest_minor_version(url, package)
     return minor_version
 
 def get_pacman_version(package):
+
+    package = package.strip()
+
+    if package in pkgname_to_arch:
+        package = pkgname_to_arch[package]
+
     command = ['pacman', '-Si', package]
     
     out = get_bash_out(command).decode()
@@ -117,21 +141,7 @@ def fill_color(original, string, spaces):
         string += " "
     return string
 
-
-def list_packages():
-    packages = os.listdir(os.getcwd())
-    packages.sort()
-
-    longest = find_longest(packages)
-
-    print(
-          fill("", longest),
-          fill("Local", 10),
-          fill("Arch", 10),
-          "GNOME")
-
-    for package in packages:
-      if os.path.isdir(package) and package != ".git":
+def print_package(package, longest):
         local = get_local_version(package)
         arch = get_pacman_version(package)
         upstream = get_upstream_version(package)
@@ -149,9 +159,27 @@ def list_packages():
           fill_color(local, local_color, 10),
           fill_color(arch, arch_color, 10),
           upstream_color)
+
+def list_packages():
+    packages = os.listdir(os.getcwd())
+    packages.sort()
+
+    longest = find_longest(packages)
+
+    print(
+          fill("", longest),
+          fill("Local", 10),
+          fill("Arch", 10),
+          "GNOME")
+
+    for package in packages:
+      if os.path.isdir(package) and package != ".git":
+          print_package(package, longest)
+
     
 
 list_packages()
 #print(get_pacman_version("gtk-xfce-engine"))
-#print(get_upstream_version("nautilus"))
+#print(get_upstream_version("gdk-p"))
+#print_package("gdk-pixbuf2", 30)
 #embed()
